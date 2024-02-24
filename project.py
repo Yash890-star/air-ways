13a2da318d9c2e894f75adf0bf7c4df0b6c1ac78
Yash890-star <yashwanthk523@gmail.com>
2024-02-24 21:43:49 +0530

    added voice recognition and improved mouse movement speed

--- project.py
+++ project.py
@@ -2,10 +2,17 @@ import mediapipe
 import cv2
 import pyautogui
 from numba import jit, cuda
+import math
+import mouse
+from concurrent.futures import ThreadPoolExecutor
+import speech_recognition as sr
+import keyboard
+import threading
+import time
 
 
 mpHands = mediapipe.solutions.hands
-captureHands = mediapipe.solutions.hands.Hands(static_image_mode=False)
+captureHands = mediapipe.solutions.hands.Hands(static_image_mode=False, max_num_hands=1,  model_complexity=0, min_detection_confidence=0.85, min_tracking_confidence=0.8)
 drawingOptions = mediapipe.solutions.drawing_utils
 screenWidth, screenHeight = pyautogui.size()
 camera = cv2.VideoCapture(0)
@@ -14,6 +21,7 @@ imageHeight = 480
 imageWidth = 640
 
 indexX = indexY = 0
+indexKnuckleX = indexKnuckleY = 0
 thumbX = thumbY = 0
 middleX = middleY = 0
 ringX = ringY = 0
@@ -24,6 +32,8 @@ mousex = mousey = 0
 def calculateAndStoreLandmarks(oneHandLandmark):
     global indexX
     global indexY
+    global indexKnuckleX
+    global indexKnuckleY
     global thumbX
     global thumbY
     global middleX
@@ -35,6 +45,9 @@ def calculateAndStoreLandmarks(oneHandLandmark):
     
     indexX = int(oneHandLandmark[8].x * imageWidth)
     indexY = int(oneHandLandmark[8].y * imageHeight)
+    
+    indexKnuckleX = int(oneHandLandmark[5].x * imageWidth)
+    indexKnuckleY = int(oneHandLandmark[5].y * imageHeight)
     
     thumbX = int(oneHandLandmark[4].x * imageWidth)
     thumbY = int(oneHandLandmark[4].y * imageHeight)
@@ -48,6 +61,67 @@ def calculateAndStoreLandmarks(oneHandLandmark):
     wristX = int(oneHandLandmark[0].x * imageWidth)
     wristY = int(oneHandLandmark[0].y * imageHeight)
     
+def type_text(text):
+    keyboard.write(text)
+
+def press_key(key):
+    threading.Thread(target=keyboard.press_and_release, args=(key,)).start()
+
+def hold_key(key):
+    keyboard.press(key)
+
+def release_key(key):
+    keyboard.release(key)
+
+def voiceCommandMode():
+    recognizer = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone(device_index=1) as mic:
+                print("Listening for a command...")
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+
+            command = recognizer.recognize_google(audio).lower()
+            print(f"Recognized command: {command}")
+
+            if "type" in command:
+                text_to_type = command.split("type", 1)[1].strip()
+                type_text(text_to_type)
+
+            elif "press" in command:
+                key_to_press = command.split("press", 1)[1].strip()
+                press_key(key_to_press.replace(" ", "+"))
+
+            elif "hold" in command:
+                key_to_hold = command.split("hold", 1)[1].strip()
+                hold_key(key_to_hold.replace(" ", "+"))
+
+            elif "release" in command:
+                key_to_release = command.split("release", 1)[1].strip()
+                release_key(key_to_release.replace(" ", "+"))
+
+            elif "exit" in command:
+                print("Exiting the program.")
+                break
+
+            else:
+                print("Invalid command. Please try again.")
+
+            # Sleep to allow the separate thread to run
+            time.sleep(1)
+
+        except sr.UnknownValueError:
+            print("Speech Recognition could not understand audio.")
+        except KeyboardInterrupt:
+            print("Program interrupted by user.")
+            break    
+    
+
+def calculateDistance(x1, x2, y1, y2):
+    return abs(math.sqrt((x2 - x1)**2 + (y2 - y1)**2))
+    
      
 def validateMousePosition(mousex, mousey):
     if mousex <= 1:
@@ -60,31 +134,33 @@ def validateMousePosition(mousex, mousey):
         mousey = 1075
     return (mousex, mousey)
 
+# rectangle size in image = 230 x 300
+
 def moveMouse():
     global mousex
     global mousey
-    mousex = int(screenWidth / imageWidth * indexX * 1)
-    mousey = int(screenHeight / imageHeight * indexY * 1)
+    mousex = int(screenWidth / (450) * indexX * 1)
+    mousey = int(screenHeight / (300) * indexY * 1)
     mousex, mousey = validateMousePosition(mousex, mousey)
-    pyautogui.moveTo(mousex, mousey)
+    # pyautogui.moveTo(mousex, mousey)
+    mouse.move(mousex, mousey, absolute=True)
 
 def scrollUp():
-    if wristY - middleY < 50:
+    if calculateDistance(wristX, middleX, wristY, middleY) < 50:
         pyautogui.scroll(100)
         
 def scrollDown():
-    if wristY - ringY < 50:
+    if calculateDistance(wristX, ringX, wristY, ringY) < 50:
         pyautogui.scroll(-100)
 
 def click():
-    if middleX - thumbX < 15:
+    if calculateDistance(indexKnuckleX, thumbX, indexKnuckleY, thumbY) < 15:
         pyautogui.click()
 
 def drag():
-    print(middleX - indexX)
-    if middleX - indexX < 10:
+    if calculateDistance(middleX, indexX, middleY, indexY) < 10:
         pyautogui.mouseDown()
-    if middleX - indexX > 30:
+    if calculateDistance(middleX, indexX, middleY, indexY):
         pyautogui.mouseUp()
 
 @jit(target_backend='cuda')
@@ -93,7 +169,9 @@ def capture():
         _, image = camera.read()
         if image is None:
             break
+        # image = cv2.resize(image, (640, 360), interpolation=cv2.INTER_LINEAR)
         image = cv2.flip(image, 1)
+        cv2.rectangle(image, (0, 0), (450, 300), (255, 0, 0), 1)
         rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         outputHands = captureHands.process(rgbImage)
         allHands = outputHands.multi_hand_landmarks
@@ -102,7 +180,8 @@ def capture():
                 drawingOptions.draw_landmarks(image, hand)
                 oneHandLandmark = hand.landmark
                 calculateAndStoreLandmarks(oneHandLandmark)
-                moveMouse()
+                if indexX <= 450 and indexY <= 300:
+                    moveMouse()
                 scrollUp()
                 scrollDown()
                 click()
